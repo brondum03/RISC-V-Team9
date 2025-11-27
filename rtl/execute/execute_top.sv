79bdb4baf1e07e876b2d46183fe9a22471049718
brondum03 <brandonauyongth@gmail.com>
2025-11-27 16:58:00 +0000

    updated alu

--- rtl/execute/execute_top.sv
+++ rtl/execute/execute_top.sv
@@ -3,21 +3,29 @@
 module execute_top #(
     parameter DATA_WIDTH = 32
 )(
-    input   logic [DATA_WIDTH-1:0]  SrcA,
-    input   logic [DATA_WIDTH-1:0]  SrcB,
+    input   logic [DATA_WIDTH-1:0]  RD1,
+    input   logic [DATA_WIDTH-1:0]  RD2,
+    input   logic [DATA_WIDTH-1:0]  ImmExt,
     input   logic [2:0]             ALUControl,
+    input   logic                   ALUSrc,
+
     output  logic [DATA_WIDTH-1:0]  ALUResult,
+    output  logic [DATA_WIDTH-1:0]  WriteData,
     output  logic                   Zero
+    
 );
 
     alu #(
         .DATA_WIDTH(DATA_WIDTH)
-    ) ALU_inst (
-        .ALUop1(SrcA),
+    ) ALU (
+        .ALUop1(RD1),
         .ALUop2(SrcB),
         .ALUctrl(ALUControl),
         .ALUout(ALUResult),
         .EQ(Zero)
     );
+
+    assign SrcB = ALUSrc ? ImmExt : RD2;
+    assign WriteData = RD2; 
 
 endmodule
