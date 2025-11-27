7d057b3c67e1a7966754b43863a85ac80fcad8b6
EZEKIEL-FROSTY <limezekiel9@gmail.com>
2025-11-27 11:49:05 +0000

    Deleted mux and added dependency on top.sv

--- rtl/top.sv
+++ rtl/top.sv
@@ -1,3 +1,8 @@
+`include "./fetch/fetch_top.sv"
+`include "./decode/decode_top.sv"
+`include "./execute/execute_top.sv"
+`include "./memory/memory_top.sv"
+
 module top #(
     parameter DATA_WIDTH = 32,
     parameter ADDR_WIDTH = 5
