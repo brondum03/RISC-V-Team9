43c34040c694357aafa2c7637ab6b8d1d54983e5
brondum03 <brandonauyongth@gmail.com>
2025-11-28 15:45:27 +0000

    fixed data memory, included addr mode

--- rtl/memory/datamemory.sv
+++ rtl/memory/datamemory.sv
@@ -1,19 +1,42 @@
 module datamemory #(
-    parameter DATA_WIDTH = 32 
+    parameter DATA_WIDTH = 32,
+    parameter BYTE_WIDTH = 8
 )(    
-    input   logic clk,
-    input   logic WE,
-    input   logic [DATA_WIDTH-1:0] WD,
-    input   logic [DATA_WIDTH-1:0] A,
-    output  logic [DATA_WIDTH-1:0] RD
-);
-    logic [DATA_WIDTH-1:0] memory [2**DATA_WIDTH-1:0];
+    input   logic [DATA_WIDTH-1:0]  write_data,
+    input   logic [DATA_WIDTH-1:0]  address,
+    input   logic                   addr_mode,  //0 for word, 1 for byte
+    input   logic                   write_enable,
+    input   logic                   clk,
+    output  logic [DATA_WIDTH-1:0]  read_data
     
-    always_ff @(posedge clk) begin
-        if (WE)
-            memory[A] <= WD;
+    
+);
+    logic [BYTE_WIDTH-1:0] memory [32'h0001FFFF:0]; 
+    
+    initial begin
+        $readmemh("data.hex", memory, 32'h00010000); 
+    end;
+
+    always_comb begin
+        if(addr_mode) begin //byte addressing
+            read_data = {24'b0, memory[address[16:0]]}; //extends the 8 bits(byte) to 32 bits with 0
+        end else begin
+            read_data = {memory[address[16:0]+3], memory[address[16:0]+2], memory[address[16:0]+1], memory[address[16:0]]};
+        end
     end
 
-    assign RD = memory[A];        
-    
+    always_ff @(posedge clk) begin
+        if (write_enable) begin
+            if (addr_mode) begin //byte mode
+                memory[address[16:0]] <= write_data[7:0];
+            end
+            else begin
+                memory[address[16:0]] <= write_data[7:0];
+                memory[address[16:0]+1] <= write_data[15:8];
+                memory[address[16:0]+2] <= write_data[23:16];
+                memory[address[16:0]+3] <= write_data[31:24];
+            end
+        end 
+    end
+
 endmodule
--- rtl/memory/memory_top.sv
+++ rtl/memory/memory_top.sv
@@ -1,19 +1,23 @@
 module memory_top #(
-    parameter DATA_WIDTH = 32
+    parameter DATA_WIDTH = 32,
+    parameter BYTE_WIDTH = 8
 )(
     input   logic [DATA_WIDTH-1:0]  ALUResult,
     input   logic [DATA_WIDTH-1:0]  WriteData,
     input   logic                   MemWrite,
+    input   logic                   AddressingMode,
     input   logic                   clk,
     output  logic [DATA_WIDTH-1:0]  ReadData
 );
     datamemory #(
-        .DATA_WIDTH(DATA_WIDTH)
+        .DATA_WIDTH(DATA_WIDTH),
+        .BYTE_WIDTH(BYTE_WIDTH)
     ) Data_Memory (
         .clk(clk),
-        .WE(MemWrite),
-        .WD(WriteData),
-        .A(ALUResult),
-        .RD(ReadData)
+        .write_enable(MemWrite),
+        .write_data(WriteData),
+        .address(ALUResult),
+        .addr_mode(AddressingMode),
+        .read_data(ReadData)
     );
 endmodule
