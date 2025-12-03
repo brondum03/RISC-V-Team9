a9cd9e0ef78b4647786e373ca8637f5a13f9bc8e
brondum03 <brandonauyongth@gmail.com>
2025-12-03 15:07:42 +0000

    control and data hazards

--- rtl/execute/hazard_unit.sv
+++ rtl/execute/hazard_unit.sv
@@ -1,7 +1,9 @@
 module hazard_unit(
+    // read from register addresses for instruction in execute stage
     input logic [4:0]   Rs1E,
     input logic [4:0]   Rs2E,
     
+    // write to register addresses for instructions in memory and writeback stages
     input logic [4:0]   RdM,
     input logic [4:0]   RdW,
 
@@ -11,13 +13,28 @@ module hazard_unit(
     input logic         RegWriteM,
     input logic         RegWriteW,  
 
+    input logic [1:0]   ResultSrcE,
+    input logic [4:0]   RdE,
+    input logic [4:0]   Rs1D,
+    input logic [4:0]   Rs2D,
+    input logic         PCSrcE,  // 1 when branch is taken (for control hazard)
+
     // forwarding
     output logic [1:0]  ForwardAE,
-    output logic [1:0]  ForwardBE
+    output logic [1:0]  ForwardBE,
+    
+    // stall and flush signals
+    output logic        StallF,
+    output logic        StallD,
+    output logic        FlushD,
+    output logic        FlushE
 );
+    
+    logic lwStall;
 
     always_comb begin
-    
+
+    // forwarding logic          
     if((Rs1E == RdM) & RegWriteM & (Rs1E!=0))   // forward from memory stage 
         ForwardAE = 2'b10;
     else if((Rs1E == RdW) & RegWriteW & (Rs1E!=0))  // forward from writeback stage
@@ -32,6 +49,15 @@ module hazard_unit(
     else 
         ForwardBE = 2'b00;    
     
+    // stall logic
+    lwStall = ResultSrcE[0] & ((Rs1D == RdE) | (Rs2D == RdE));  //resultsrcE[0] = 1 for load instructions
+    StallF = lwStall;
+    StallD = lwStall;
+    
+    //flush logic
+    FlushD = lwStall;
+    FlushE = lwStall | PCSrcE;  // flush execute stage on branch taken, or load-use hazard
+
     end
 
 endmodule
